6366ccb644ffa97e87ebd7cbf69feff7ae7d4dd9
Rony Arya Wijaya <rony.wijaya@ccn.com.sg>
2025-02-11 10:02:35 +0700

    migrate from template html to React component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Browse from './components/Browse';
+import Arrived from './components/Arrived';
+import Client from './components/Client';
+import AsideMenu from './components/AsideMenu';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+    <Header />
+      <Hero />
+      <Browse />
+      <Arrived />
+      <Client />
+      <AsideMenu />
+      <Footer/>
     </div>
   );
 }
