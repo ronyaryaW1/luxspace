c920deb36eb3b67829e21b874198ad5103f35d1b
Rony Arya Wijaya <rony.wijaya@ccn.com.sg>
2025-02-17 17:23:39 +0700

    add splash screen

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Browse from './components/Browse';
@@ -6,69 +6,77 @@ import Arrived from './components/Arrived';
 import Client from './components/Client';
 import AsideMenu from './components/AsideMenu';
 import Footer from './components/Footer';
-
+import Splash from './pages/Splash';
 
 function App() {
-  const [items, setItems] = useState([])
-  // const [offlineStatus, setOfflineStatus] = useState(navigator.onLine)
+	const [items, setItems] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	// const [offlineStatus, setOfflineStatus] = useState(navigator.onLine)
 
-  // function handleOfflineStatus (){
-  //   setOfflineStatus(!offlineStatus)
-  // }
+	// function handleOfflineStatus (){
+	//   setOfflineStatus(!offlineStatus)
+	// }
 
-  // useEffect(() => {
-  //   window.addEventListener('offline', handleOfflineStatus())
-  //   window.addEventListener('online', handleOfflineStatus())
+	// useEffect(() => {
+	//   window.addEventListener('offline', handleOfflineStatus())
+	//   window.addEventListener('online', handleOfflineStatus())
 
-  //   return function () {
-  //     window.removeEventListener('offline', handleOfflineStatus())
-  //     window.removeEventListener('online', handleOfflineStatus())
+	//   return function () {
+	//     window.removeEventListener('offline', handleOfflineStatus())
+	//     window.removeEventListener('online', handleOfflineStatus())
 
-  //   }
+	//   }
   // }, [])
+  
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/posts",
-          {
-            headers: {
-              "Content-Type": "application/json",
-              Accept: "application/json",
-            },
-          }
-        );
-        const data = await response.json();
-        setItems(data.slice(0, 5))
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+					headers: {
+						'Content-Type': 'application/json',
+						Accept: 'application/json',
+					},
+				});
+				const data = await response.json();
+				setItems(data.slice(0, 5));
+			} catch (error) {
+				console.error('Error fetching data:', error);
+			}
+		};
 
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    
-    fetchData().then(() => {
-      if (!document.querySelector("script[src='/carousel.js']")) {
-        const script = document.createElement("script");
-        script.src = "/carousel.js";
-        script.async = false;
-        document.body.appendChild(script);
-      }
-    });
-  }, []);
 
-  return (
-    <div>
-     
-    <Header />
-      <Hero />
-      <Browse />
-      <Arrived items={items} />
-      <Client />
-      <AsideMenu />
-      <Footer/>
-    </div>
-  );
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1500);
+
+		fetchData().then(() => {
+			if (!document.querySelector("script[src='/carousel.js']")) {
+				const script = document.createElement('script');
+				script.src = '/carousel.js';
+				script.async = false;
+				document.body.appendChild(script);
+			}
+		});
+	}, []);
+
+	return (
+		<div>
+			{isLoading ? (
+				<Splash />
+			) : (
+				<>
+					<Header />
+					<Hero />
+					<Browse />
+					<Arrived items={items} />
+					<Client />
+					<AsideMenu />
+					<Footer />
+				</>
+			)}
+		</div>
+	);
 }
 
 export default App;
